1b42dd442f81577afc8aab0d70848610ec07dd77
Jackie <jackiywijaya50@mhs.mdp.ac.id>
2023-04-09 12:55:06 +0700

    Create User.java

---
+++ app/src/main/java/jackie/kontak/db/User.java
@@ -0,0 +1,23 @@
+package jackie.kontak.db;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "users")
+public class User {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @ColumnInfo(name = "email")
+    private String email;
+
+    @ColumnInfo(name = "phone")
+    private String phone;
+
+    public User() {
+    }
+}
